0dfbd1799e96788a979facd15f77918be80c7230
Matt Taylor <taylor.matt777@gmail.com>
2019-12-09 09:29:29 -0700

    Finished day 5 part 2, might come back later to clean up for day 7

--- day05/day05.js
+++ day05/day05.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const { clone } = require('lodash');
 
+const DEBUG = false;
+
 const HALT = 99;
 const ADD = 1;
 const MULT = 2;
@@ -14,21 +16,15 @@ const EQUALS = 8;
 const POSITION_MODE = 0;
 const IMMEDIATE_MODE = 1;
 
-function part1(instructions) {
-  getPosition0(instructions, 1);
-}
-
-function part2(instructions) {
-  getPosition0(instructions, 5);
-}
-
 /**
  * Given the initialInstructions, and an input, run the program and return 
  * the value at position 0 when the computer halts
  */
-function getPosition0(initialInstructions, input) {
+function runIntComputer(initialInstructions, input) {
   const instructions = clone(initialInstructions)
   let position = 0;
+
+  if (DEBUG) console.log(`Instructions:\n${instructions.join(',')}\n`);
   
   while (position < instructions.length) {
     let operation = String(instructions[position]);
@@ -37,15 +33,14 @@ function getPosition0(initialInstructions, input) {
     // operation '1001' for example becomes '01' -> 1 -> ADD
     const opcode = parseInt(operation.slice(operation.length - 2));
 
-    console.log(`operation: ${operation}, opcode: ${opcode}`);
-
     // handle HALT operation immediately
     if (opcode === HALT) {
+      if (DEBUG) console.log('HALT');
       return instructions[0];
     } 
     
     // handle operations
-    if (opcode === ADD) {
+    else if (opcode === ADD) {
       const pos1 = instructions[position + 1];
       const pos2 = instructions[position + 2];
       const savePos = instructions[position + 3];
@@ -55,8 +50,11 @@ function getPosition0(initialInstructions, input) {
       const param2Val = getParamMode(operation, 1) === POSITION_MODE ? instructions[pos2] : pos2;
       const result = param1Val + param2Val;
 
-      // wtf does the savePos do in immediate mode? doing the same thing here
-      getParamMode(operation, 2) === POSITION_MODE ? instructions[savePos] = result : instructions[savePos] = result;
+      // wtf does the savePos do in immediate mode?
+      instructions[savePos] = result;
+
+      if (DEBUG) console.log(`${operation},${pos1},${pos2},${savePos} -> ${param1Val}+${param2Val}=${result} -> Store at ${savePos}`);
+      if (DEBUG) console.log(instructions.join(','));
 
       // 1 opcode, 3 params
       position += 4;
@@ -72,8 +70,11 @@ function getPosition0(initialInstructions, input) {
       const param2Val = getParamMode(operation, 1) === POSITION_MODE ? instructions[pos2] : pos2;
       const result = param1Val * param2Val;
 
-      // wtf does the savePos do in immediate mode? doing the same thing here
-      getParamMode(operation, 2) === POSITION_MODE ? instructions[savePos] = result : instructions[savePos] = result;
+      // wtf does the savePos do in immediate mode?
+      instructions[savePos] = result;
+
+      if (DEBUG) console.log(`${operation},${pos1},${pos2},${savePos} -> ${param1Val}+${param2Val}=${result} -> Store at ${savePos}`);
+      if (DEBUG) console.log(instructions.join(','));
 
       // 1 opcode, 3 params
       position += 4;
@@ -86,13 +87,17 @@ function getPosition0(initialInstructions, input) {
       const pos = instructions[position + 1];
       instructions[pos] = input;
 
+      if (DEBUG) console.log(`${operation},${pos} -> Store ${input} at ${pos}`);
+      if (DEBUG) console.log(instructions.join(','));
+
       // 1 opcode, 1 param
       position += 2;
     } 
     
     else if (opcode === OUTPUT) {
-      const pos = instructions[position + 1];
-      console.log(`OUTPUT: ${instructions[pos]}`);
+      const pos1 = instructions[position + 1];
+      const param1Val = getParamMode(operation, 0) === POSITION_MODE ? instructions[pos1] : pos1;
+      console.log(`\n${operation},${pos1} - > OUTPUT: ${param1Val}\n`);
 
       // 1 opcode, 1 param
       position += 2;
@@ -100,32 +105,48 @@ function getPosition0(initialInstructions, input) {
     
     else if (opcode === JUMP_IF_TRUE) {
       const pos1 = instructions[position + 1];
-      const pos2 = instructions[position + 1];
+      const pos2 = instructions[position + 2];
 
       const param1Val = getParamMode(operation, 0) === POSITION_MODE ? instructions[pos1] : pos1;
       const param2Val = getParamMode(operation, 1) === POSITION_MODE ? instructions[pos2] : pos2;
 
+      if (DEBUG) console.log(`${operation},${pos1},${pos2} -> ${param1Val} ${param1Val === 0 ? '===' : '!=='} 0`);
+
+      // Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction 
+      // pointer to the value from the second parameter. Otherwise, it does nothing.
       if (param1Val !== 0) {
         position = param2Val;
+        if (DEBUG) console.log(`Jump to ${position}`);
       } else {
         // 1 opcode, 2 params
         position += 3;
+        if (DEBUG) console.log(`Do nothing`);
       }
+
+      if (DEBUG) console.log(instructions.join(','));
     }
 
     else if (opcode === JUMP_IF_FALSE) {
       const pos1 = instructions[position + 1];
-      const pos2 = instructions[position + 1];
+      const pos2 = instructions[position + 2];
 
       const param1Val = getParamMode(operation, 0) === POSITION_MODE ? instructions[pos1] : pos1;
       const param2Val = getParamMode(operation, 1) === POSITION_MODE ? instructions[pos2] : pos2;
 
-      if (param1Val == 0) {
+      if (DEBUG) console.log(`${operation},${pos1},${pos2} -> ${param1Val} ${param1Val === 0 ? '===' : '!=='} 0`);
+
+      // Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction 
+      // pointer to the value from the second parameter. Otherwise, it does nothing.
+      if (param1Val === 0) {
         position = param2Val;
+        if (DEBUG) console.log(`Jump to ${position}`);
       } else {
         // 1 opcode, 2 params
         position += 3;
+        if (DEBUG) console.log(`Do nothing`);
       }
+
+      if (DEBUG) console.log(instructions.join(','));
     }
 
     else if (opcode === LESS_THAN) {
@@ -138,8 +159,11 @@ function getPosition0(initialInstructions, input) {
       const param2Val = getParamMode(operation, 1) === POSITION_MODE ? instructions[pos2] : pos2;
       const result = param1Val < param2Val ? 1 : 0;
 
-      // wtf does the savePos do in immediate mode? doing the same thing here
-      getParamMode(operation, 2) === POSITION_MODE ? instructions[savePos] = result : instructions[savePos] = result;
+      // wtf does the savePos do in immediate mode? 
+      instructions[savePos] = result;
+
+      if (DEBUG) console.log(`${operation},${pos1},${pos2},${savePos} -> ${param1Val} ${result === 1 ? '<' : '>='} ${param2Val} -> result: ${result} -> Store at ${savePos}`);
+      if (DEBUG) console.log(instructions.join(','));
 
       // 1 opcode, 3 params
       position += 4;
@@ -155,8 +179,11 @@ function getPosition0(initialInstructions, input) {
       const param2Val = getParamMode(operation, 1) === POSITION_MODE ? instructions[pos2] : pos2;
       const result = param1Val === param2Val ? 1 : 0;
 
-      // wtf does the savePos do in immediate mode? doing the same thing here
-      getParamMode(operation, 2) === POSITION_MODE ? instructions[savePos] = result : instructions[savePos] = result;
+      // wtf does the savePos do in immediate mode?
+      instructions[savePos] = result;
+
+      if (DEBUG) console.log(`${operation},${pos1},${pos2},${savePos} -> ${param1Val} ${result === 1 ? '===' : '!=='} ${param2Val} -> result: ${result} -> Store at ${savePos}`);
+      if (DEBUG) console.log(instructions.join(','));
 
       // 1 opcode, 3 params
       position += 4;
@@ -191,6 +218,8 @@ function parseInput() {
 }
 
 // parse input and output answers
-var input = parseInput();
-console.log(part1(input));
-console.log(part2(input));
+var instructions = parseInput();
+console.log('PART 1:')
+runIntComputer(instructions, 1);
+console.log('PART 2:')
+runIntComputer(instructions, 5);
